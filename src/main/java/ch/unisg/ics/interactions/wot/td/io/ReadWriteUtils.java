c6f9417916196b6721a5309783e7434430b88d59
Kai <112092835+KaiTries@users.noreply.github.com>
2024-11-26 14:34:36 +0100

    figuring out json

--- src/main/java/ch/unisg/ics/interactions/wot/td/io/ReadWriteUtils.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/io/ReadWriteUtils.java
@@ -17,12 +17,6 @@ import javax.xml.parsers.DocumentBuilder;
 import no.hasmac.jsonld.JsonLdError;
 import no.hasmac.jsonld.document.Document;
 import no.hasmac.jsonld.document.JsonDocument;
-import org.apache.commons.rdf.api.Graph;
-import org.apache.commons.rdf.api.IRI;
-import org.apache.commons.rdf.api.RDFSyntax;
-import org.apache.commons.rdf.rdf4j.RDF4J;
-import org.apache.commons.rdf.rdf4j.RDF4JGraph;
-import org.apache.commons.rdf.rdf4j.RDF4JTriple;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 import org.eclipse.rdf4j.model.vocabulary.RDFS;
@@ -41,7 +35,6 @@ import org.eclipse.rdf4j.rio.helpers.StatementCollector;
 
 public final class ReadWriteUtils {
   private final static Logger LOGGER = Logger.getLogger(ReadWriteUtils.class.getCanonicalName());
-  private final static RDF4J rdfImpl = new RDF4J();
 
 
   public static Model loadModel(RDFFormat format, String representation, String baseURI) {
@@ -75,99 +68,6 @@ public final class ReadWriteUtils {
     rdfParser.parse(stringReader, baseURI);
     
     return model;
-  }
-
-  public static Graph stringToGraph(String graphString, IRI baseIRI, RDFSyntax syntax) throws IllegalArgumentException, IOException {
-    StringReader stringReader = new StringReader(graphString);
-
-    RDFFormat format = RDFFormat.JSONLD;
-    if (syntax.equals(RDFSyntax.TURTLE)) {
-      format = RDFFormat.TURTLE;
-    }
-
-    RDFParser rdfParser = Rio.createParser(format);
-    Model model = new LinkedHashModel();
-    rdfParser.set(JSONLDSettings.SECURE_MODE, false);
-    // rdfParser.set(JSONLDSettings.EXCEPTION_ON_WARNING, true);
-    rdfParser.setRDFHandler(new StatementCollector(model));
-
-    try {
-      rdfParser.parse(stringReader, null);
-    }
-    catch (RDFParseException e) {
-      throw new IllegalArgumentException("RDF parse error: " + e.getMessage());
-    }
-    catch (RDFHandlerException e) {
-      throw new IOException("RDF handler exception: " + e.getMessage());
-    }
-    finally {
-      stringReader.close();
-    }
-    return rdfImpl.asGraph(model);
-  }
-
-
-  public static String graphToString(Graph graph, RDFSyntax syntax) throws IllegalArgumentException,
-      IOException {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-    RDFWriter writer;
-
-    if (syntax.equals(RDFSyntax.TURTLE)) {
-      writer = Rio.createWriter(RDFFormat.TURTLE, out);
-    } else if (syntax.equals(RDFSyntax.JSONLD)) {
-      writer = Rio.createWriter(RDFFormat.JSONLD, out);
-      writer.getWriterConfig().set(JSONLDSettings.JSONLD_MODE, JSONLDMode.FLATTEN);
-      writer.getWriterConfig().set(JSONLDSettings.USE_NATIVE_TYPES, true);
-      writer.getWriterConfig().set(JSONLDSettings.OPTIMIZE, true);
-    } else {
-      throw new IllegalArgumentException("Unsupported RDF serialization format.");
-    }
-
-    writer.getWriterConfig()
-        .set(BasicWriterSettings.PRETTY_PRINT, true)
-        .set(BasicWriterSettings.RDF_LANGSTRING_TO_LANG_LITERAL, true)
-        .set(BasicWriterSettings.XSD_STRING_TO_PLAIN_LITERAL, true)
-        .set(BasicWriterSettings.INLINE_BLANK_NODES, true);
-
-    if (graph instanceof RDF4JGraph) {
-      try {
-        writer.startRDF();
-
-        writer.handleNamespace("eve", "http://w3id.org/eve#");
-        writer.handleNamespace("td", "https://www.w3.org/2019/wot/td#");
-        writer.handleNamespace("htv", "http://www.w3.org/2011/http#");
-        writer.handleNamespace("hctl", "https://www.w3.org/2019/wot/hypermedia#");
-        writer.handleNamespace("wotsec", "https://www.w3.org/2019/wot/security#");
-        writer.handleNamespace("dct", "http://purl.org/dc/terms/");
-        writer.handleNamespace("js", "https://www.w3.org/2019/wot/json-schema#");
-        writer.handleNamespace("saref", "https://w3id.org/saref#");
-
-        try (Stream<RDF4JTriple> stream = ((RDF4JGraph) graph).stream()) {
-          stream.forEach(triple -> {
-            writer.handleStatement(triple.asStatement());
-          });
-        }
-        writer.endRDF();
-      }
-      catch (RDFHandlerException e) {
-        throw new IOException("RDF handler exception: " + e.getMessage());
-      }
-      catch (UnsupportedRDFormatException e) {
-        throw new IllegalArgumentException("Unsupported RDF syntax: " + e.getMessage());
-      }
-      finally {
-        out.close();
-      }
-    } else {
-      throw new IllegalArgumentException("Unsupported RDF graph implementation");
-    }
-    return out.toString();
-  }
-
-
-  public static String jsonStrintoRdfString(final String input) throws IOException {
-    return graphToString(stringToGraph(input, null, RDFSyntax.JSONLD),RDFSyntax.TURTLE);
   }
 
   private static Document getFixedContext () throws URISyntaxException, IOException, JsonLdError {
